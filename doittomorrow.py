21fbd1b0d2bf16a39dbd7fb75e7c5ca704fe72f1
Rex Tsai (蔡志展) <rex.cc.tsai@gmail.com>
2017-06-06 15:46:24 +0800

    Fix for new API

--- doittomorrow.py
+++ doittomorrow.py
@@ -13,8 +13,12 @@ import copy
 import os
 from datetime import date, timedelta
 
-from zim.formats import ParseTree, Element, TreeBuilder, ParseTreeBuilder, get_format, UNCHECKED_BOX, CHECKED_BOX, XCHECKED_BOX
-
+from zim.formats import ParseTree, Element, Visitor, get_format, UNCHECKED_BOX, CHECKED_BOX, XCHECKED_BOX
+try:
+    import xml.etree.cElementTree as ElementTreeModule
+except:  #pragma: no cover
+    logger.warn('Could not load cElementTree, defaulting to ElementTreeModuleementTree')
+    import xml.etree.ElementTree as ElementTreeModule
 def main(orig_file, notebook_path):
     assert(os.path.isdir(notebook_path))
     assert(os.path.isfile(orig_file))
@@ -24,21 +28,27 @@ def main(orig_file, notebook_path):
 
     text = open(orig_file).read()
     parsetree = parser.parse(text)
+
+    # Title
     newtree = ParseTree().fromstring("<zim-tree></zim-tree>")
 
-    for para in parsetree.findall('p'):
-        p = Element("p")
+    title = parsetree.find("h")[0];
+    if(title is not None):
+        newtree.set_heading(title, 1)
+        newtree._etree.getroot().append(ElementTreeModule.Element('p'))
+
+    for para in parsetree._etree.getiterator('p'):
+        p = ElementTreeModule.Element('p')
         for node in flatten_list(para, para):
             p.append(node)
-        newtree.getroot().extend(p)
+        newtree._etree.getroot().append(p)
 
     # new todo list
     text = ''.join(dumper.dump(newtree)).encode('utf-8')
 
     tomorrow = date.today() + timedelta(1)
-
-    directory = os.path.join(notebook_path, "Calendar", str("%04d" % tomorrow.year), str("%02d" % tomorrow.month))
-    filename = "%02d.txt" % tomorrow.day
+    directory = os.path.join(notebook_path, "Calendar", str("%04d" % tomorrow.year), str("%02d" % tomorrow.month), str("%02d" % tomorrow.day))
+    filename = "stash.txt"
     if (not os.path.exists(directory)): os.makedirs(directory)
 
     # write tasks to tomorrow page.
@@ -49,7 +59,7 @@ def main(orig_file, notebook_path):
     text = ''.join(dumper.dump(parsetree) ).encode('utf-8')
     with open(orig_file, 'w') as the_file:
          the_file.write(text)
-    
+
 def flatten_list(node, parent = None, copied = False):
     for child in node.getchildren():
         if child.tag in ('li') and child.get('bullet') not in (CHECKED_BOX, XCHECKED_BOX):
